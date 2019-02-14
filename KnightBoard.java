e2c8a15b40d2387c230beb13eb5eaf959b515dd5
Qiong <qhuang00@stuy.edu>
2019-02-14 11:06:31 -0500

    fixed solve and wrote counts olutions

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,6 @@
 public class KnightBoard{
   private int[][] board;
-  private static final int[][] directions = {{1,2},{2,1},{2,-1},{-2,1},{-1,-2},{-2,-1},{-2,1},{-1,2}};
+  private static final int[][] directions = {{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1},{-2,1},{-1,2}};
   public KnightBoard(int startingRows,int startingCols){
     if(startingCols < 0 || startingCols < 0){
       throw new IllegalArgumentException();
@@ -11,8 +11,8 @@ public class KnightBoard{
     String s = "";
     for (int r = 0; r < board.length ; r++ ) {
       for (int c = 0; c < board[0].length ;c++ ) {
-        if(board[r][c] < 0){
-          s += "\n";
+        if(board[r][c] < 10){
+          s += " ";
         }
         s += board[r][c];
         if(c != board[0].length - 1){
@@ -39,20 +39,51 @@ public class KnightBoard{
 
     return solveH(startingRows,startingCols,0);
   }
-  public boolean solveH(int row, int col, int level){
-    if(level == board.length * board[0].length){
+
+  public int countSolutions(int startRow, int startCol){
+    if(startRow < 0 || startCol < 0){
+      throw new IllegalArgumentException();
+    }
+    for (int[] row :board ) {
+      for (int num: row){
+        if(num != 0 ){
+          throw new IllegalStateException();
+        }
+      }
+    }
+    return countSolutionsR(0,0,0);
+  }
+
+  private boolean solveH(int row, int col, int level){
+    board[row][col] = level + 1;
+    if(level + 1 == board.length * board[0].length){
       return true;
     }
     for (int[] direction : directions) {
       if(row + direction[0] >= 0 && row + direction[0] < board.length
-        && col + direction[1] >= 0 && col + direction[1] < board[0].length){
-          board[row + direction[0]][col + direction[1]] = level +1;
-          if(solveH(row + direction[0], col + direction[1], level + 1)){
+        && col + direction[1] >= 0 && col + direction[1] < board[0].length &&
+        board[row + direction[0]][col + direction[1]] == 0 &&
+        solveH(row + direction[0], col + direction[1], level + 1)){
             return true;
-          }
-          //board[row + direction[0]][col + direction[1]] = 0;
         }
     }
+    board[row][col] = 0;
     return false;
+  }
+  private int countSolutionsR(int row, int col, int level){
+    int sols = 0;
+    board[row][col] = level + 1;
+    if(level + 1 == board.length * board[0].length){
+      sols++;
+    }
+    for (int[] direction : directions) {
+      if(row + direction[0] >= 0 && row + direction[0] < board.length
+        && col + direction[1] >= 0 && col + direction[1] < board[0].length &&
+        board[row + direction[0]][col + direction[1]] == 0){
+            sols += countSolutionsR(row + direction[0], col + direction[1], level + 1);
+        }
+    }
+    board[row][col] = 0;
+    return sols;
   }
 }
